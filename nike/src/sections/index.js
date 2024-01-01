cd28c0771eb93604b96650e4d3f4997aae39db5e
hftrujillo <hftrujillo@loyola.edu>
2024-01-01 01:54:03 -0500

    first commit

---
+++ nike/src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import Footer from './Footer';
+import Services from './Services';
+import SpecialOffer from './SpecialOffer';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+
+export {
+    Hero,
+    PopularProducts,
+    Subscribe,
+    SuperQuality,
+    SpecialOffer,
+    Footer,
+    CustomerReviews,
+    Services
+}
