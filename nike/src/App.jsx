c749bcf484330a4a4374330c741fc5c3434607bf
hftrujillo <hftrujillo@loyola.edu>
2024-01-01 19:44:51 -0500

    popular products section

--- nike/src/App.jsx
+++ nike/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => (
       <Hero />
     </section>
     <section className = "padding">
-      PopularProducts
+      <PopularProducts/>
     </section>
     <section className = "padding">
       SuperQuality
